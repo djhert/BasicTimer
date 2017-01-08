7c14bf3772e5c5c92bb7566c51be88ddb8af5fb9
MarkedHero <markedhero@projectstudiogames.com>
2017-01-08 12:07:41 -0500

    Better Documented Test

--- test/BasicTimerTest.go
+++ test/BasicTimerTest.go
@@ -1,17 +1,31 @@
 package main
+
+//Import the Timer
 import BasicTimer "github.com/markedhero/BasicTimer"
-import "time"
+import "time" //Used for time.Sleep(), not needed otherwise
 
 func main() {
-	t := new(BasicTimer.Timer)
-	t.Start()
+	//Create the object
+	basicTimer := new(BasicTimer.Timer)
+	//Start the timer
+	basicTimer.Start()
+	//Wait for 1 minute
 	time.Sleep(time.Second * 60)
-	t.Print()
-	t.Pause(true)
+	//Print the current time accumulated
+	basicTimer.Print()
+	//Pause the timer
+	basicTimer.Pause(true)
+	//Wait 10 seconds while paused
 	time.Sleep(time.Second * 10)
-	t.Print()
-	t.Pause(false)
+	//Show no time has been accumulated
+	basicTimer.Print()
+	//Unpause timer
+	basicTimer.Pause(false)
+	//Wait another 10 seconds
 	time.Sleep(time.Second * 10)
-	t.Print()
-	t.Stop()	
+	//Show time is being accumulated after unpausing
+	basicTimer.Print()
+	//End the timer
+	basicTimer.Stop()	
+	/* Once "Stop()" has been run, you can reset the timer by using Start() */
 }
